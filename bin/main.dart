5d9d39815b321b63a2b8820a198ba2b673800b74
Manuel de Jesús Gámez López <winmanuel09@gmail.com>
2022-04-30 12:04:49 -0600

    ejemplo de switch en Dart

---
+++ bin/main.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  int opcion;
+  opcion = 4;
+
+  switch (opcion) {
+    case 1:
+      print('Estas en la opcion 1');
+      break;
+    case 2:
+      print('Estas en la opcion 2');
+      break;
+    case 3:
+      print('Estas en la opcion 3');
+      break;
+    default:
+      print('No estas en ninguna opcion');
+      break;
+  }
+}
